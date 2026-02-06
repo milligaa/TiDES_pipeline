62a711a80ee88300df233d85077026658159b852
Andrew Milligan <a.milligan@lancaster.ac.uk>
2026-02-06 19:11:39 +0000

    fixed M25 pipeline logic

--- source/M25_pipeline.py
+++ source/M25_pipeline.py
@@ -37,9 +37,14 @@ def pipeline(config_path, spec):
     if dash_res['Best_class'][0] in ['Ia', 'II']:
         if ngsf_res['Best_class'][0] == dash_res['Best_class'][0]:
             pipeline_class = ngsf_res['Best_class'][0]
-        elif rband_mag(spec) == True:
-            print(ngsf_res['Best_class'][0])
-            pipeline_class = ngsf_res['Best_class'][0]
+        else:
+            if rband_mag(spec) == True:
+                pipeline_class = ngsf_res['Best_class'][0]
+            else:
+                pipeline_class = 'Other'
+                
+    elif rband_mag(spec) == True:
+        pipeline_class = ngsf_res['Best_class'][0]
     else:
         pipeline_class = 'Other'
 
